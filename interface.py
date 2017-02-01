4efbcc69fce8b779adb6f32a8a6e7f9f9756dc8b
zerorax <willoiffer@yahoo.com>
2017-02-01 13:58:23 -0500

    still bugging

--- interface.py
+++ interface.py
@@ -13,6 +13,9 @@ wheight = height
 wwidth = width
 window = curses.newwin(wheight,wwidth,begin_y,begin_x)
 
+window.idlok(1)
+
+
 screen.move(height-2,0)
 screen.clear()
 window.refresh()
@@ -24,27 +27,50 @@ while True:
 	elif ch == curses.KEY_ENTER or ch == 10 or ch == 13:
 		window.addstr(0,0,inbuf)
 		window.clrtoeol()
+		screen.move(height-2,0)
 		window.refresh()
 		inbuf = ""
 		continue
-	elif ch == curses.KEY_BACKSPACE or ch == 127:
-		if len(inbuf) > 0:
+	elif ch == curses.KEY_BACKSPACE or ch == 127 or ch == 800 or ch == 8:
+		if len(inbuf) > width:
+			#screen.clear()
 			inbuf = inbuf[:-1]
-			window.addstr(height-2,0,inbuf)
+			if len(inbuf) > width:
+				curbuf =  inbuf[-width:]
+				window.addstr(10,0,inbuf[-width:])
+			else:
+				curbuf = inbuf
+			#window.addstr(10,0,curbuf)
+			#screen.move(height-2,len(curbuf))
+			window.addstr(height-2,0,curbuf)
 			window.clrtoeol()
+			window.refresh()
+		elif len(inbuf) > 0:
+			screen.clear()
+			inbuf = inbuf[:-1]
+			if  len(inbuf) - width > 0:
+				screen.move(height-2,len(inbuf)-width)
+			window.addstr(height-2,0,inbuf[:width])
+			window.clrtoeol()
+			screen.move(height-2,len(inbuf))
 			window.refresh()
 	else:
 		inbuf = "%s%s" % (inbuf,str(chr(ch)))
 		if len(inbuf) >= width:
-			tempbuf = inbuf[len(inbuf) - width - 1:]
+			tempbuf = inbuf[len(inbuf) - width :]
 			window.addstr(height-2,0,tempbuf)
 			window.clrtoeol()
+			screen.move(height-2,width-1)
 			window.refresh()
-		else:
+		elif len(inbuf) < width:
+
 			window.addstr(height-2,0,inbuf)
+			screen.move(height-2,len(inbuf))
 			window.clrtoeol()
 			window.refresh()
 
+
+		window.refresh()
 
 curses.nocbreak()
 screen.keypad(0)
