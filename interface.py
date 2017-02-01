b343f305f6556708e4c0d97055773c39b57f02a5
zerorax <willoiffer@yahoo.com>
2017-02-01 02:44:33 -0500

    input kinda working

--- interface.py
+++ interface.py
@@ -10,20 +10,30 @@ curses.curs_set(1)
 
 window = curses.newwin(height-1,width-1,0,0)
 window.clrtoeol()
+
+
 cursor_position = 0
 screen.move(height-2,0)
 command_history = []
 
 screenbuffer = []
-
+command_history = []
 def send_to_screen(text):
 	screenbuffer.append(text)
+	
+def refresh_screen():
+	if len(screenbuffer) > height - 3:
+		for line, lnum in screenbuffer[height-3:],range(height-3):
+			window.addstr(lnum,0, line)
+	else:
+		for line, lnum in screenbuffer,range(len(screenbuffer)):
+			window.addstr(lnum,0, line)
+	
 
 inbuf = ""
 while True:
 	screen.move(height-2,cursor_position)
 	key = screen.getch()
-#	window.addstr(0,0,str(key))
 	if key == ord('q'):
 		exit(0)
 	elif key == curses.KEY_ENTER or key == 10 or key == 13:
@@ -37,12 +47,17 @@ while True:
 	elif key == curses.KEY_BACKSPACE or key == 127:
 		if cursor_postion > 0:
 			inbuf = inbuf[:1]
-			cursor_position -= 1
-			screen.move(height-2,cursor_position)
-			if len(inbuf) > width-1:
-				tempbuf = inbuf[len(inbuf)-width-1:]
+			if len(inbuf) >= width-1:
+				screen.move(height-2,width-1)
 			else:
-				window.addstr(height-2,0,tempbuf)		
+				cursor_position -= 1
+				screen.move(height-2,cursor_position)
+				window.addstr(height-2,0, inbuf)
+			if len(inbuf) >= width-1:
+				tempbuf = inbuf[len(inbuf)-width-2:]
+				window.addstr(height-2,0,tempbuf)
+			else:
+#				window.addstr(height-2,0,inbuf)	
 				padding = width - len(inbuf)-2
 				tempbuf = inbuf
 				for i in range(padding):
