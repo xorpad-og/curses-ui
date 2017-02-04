b3e0682706c202c07e9cfb3214376796c534e6c8
zerorax <willoiffer@yahoo.com>
2017-02-04 01:25:41 -0500

    objectifying things

--- interface.py
+++ interface.py
@@ -13,13 +13,73 @@ class InterfaceObject(object):
 		self.height,self.width=self.screen.getmaxyx()
 		self.inbuf = ""
 
+class CursesWindow(object):
+	def __init__(self,height,width,loc_y,loc_x):
+		self.window = curses.newwin(height,length,loc_y,loc_x,box=True,showcursor=False)
+		self.height = height
+		self.width = width
+		self.loc_y = loc_y
+		self.loc_x = loc_x
+		self.box = box
+		self.scrollback= []
+
+		if showcursor == False:
+			self.window.leaveok(1)
+		else:
+			self.window.leaveok(0)
+		window.keypad(0)
+
+	def resize(self,height,length):
+		self.window.resize(height,length)
+		self.height = hieght
+		self.width = width
+		#redraw the window backlog
+
+	def move(self,y,x):
+		self.loc_y = y
+		self.loc_x = x
+
+	def write(self,text):
+		if self.box == True:
+			lines = wordwrap(text,self.width-2)
+		else:
+			lines = wordwrap(text,self.width)
+		for line in lines:
+			self.scrollback.append(line)
+		if self.box == True:
+			bufferlen = self.height-2
+		else:
+			bufferlen = self.height
+
+		if self.box == True:
+			startx = 1
+		else:
+			startx = 0
+		if len(self.scrollback) < bufferlen:
+			curline = bufferlen - len(self.scrollback) + startx
+			for lnum in range(len(scrollback)):
+				if self.box == True:
+					self.window.addstr(curline,1,scrollback[lnum])
+					self.window.clrtoeol()
+				else:
+					self.window.addstr(curline,0,scrollback[lnum])
+					self.window.clrtoeol()
+			return
+		if len(self.scrollback) >= bufferlen:
+			curline = startx
+			for line in self.scrollback[:-bufferlen-startx,startx]:
+				self.window.addstr(curline,startx,line)
+				self.window.clrtoeol
+		return
+
+
 def ScreenRefresh(uiobj):
-	if uiobj.bufposition > uiobj.width and uiobj.bufposition == len(uiobj.inbuf):
+	if uiobj.bufposition > uiobj.width -2 and uiobj.bufposition == len(uiobj.inbuf):
 		uiobj.screen.move(uiobj.height-2,uiobj.width-1)
-	elif uiobj.bufposition <= uiobj.width:
+	elif uiobj.bufposition > uiobj.width-2 and uiobj.bufposition <= uiobj.width -2:
 		uiobj.screen.move(uiobj.height-2,uiobj.width-1)
 	else:
-		uiobj.screen.move(uiobj.height-2,uiobj.bufposition - uiobj.width)
+		uiobj.screen.move(uiobj.height-2,uiobj.bufposition+1)
 	uiobj.window.box()
 	uiobj.window.refresh()
 	uiobj.inputwin.box()
@@ -158,18 +218,20 @@ def InputLoop(uiobj):
 			draw_main(uiobj)
 			ScreenRefresh(uiobj)
 			continue
+
 		elif ch == curses.KEY_BACKSPACE or ch == 127 or ch == 800 or ch == 8:
 			if len(uiobj.inbuf) > uiobj.width:
 				uiobj.inbuf = uiobj.inbuf[:-1]
+				bufposition -= 1
 				if len(uiobj.inbuf)+1 < uiobj.width-1:
 					uiobj.screen.move(uiobj.height-2,len(uiobj.inbuf)+1)
 				else:
 					uiobj.screen.move(uiobj.height-2,uiobj.width-1)
-				if len(uiobj.inbuf) - uiobj.width > 0:
-					extra= len(uiobj.inbuf) - uiobj.width
+				if len(uiobj.inbuf) - uiobj.width -2 > 0:
+					extra= len(uiobj.inbuf) - uiobj.width-2
 					curbuf =  uiobj.inbuf[:-extra]
 				else:
-					curbuf =uiobj.inbuf
+					curbuf = uiobj.inbuf
 				uiobj.inputwin.addstr(1,1,curbuf[-width-2:])
 				if len(curbuf) < uiobj.width-2:
 					uiobj.screen.move(uiobj.height-2,len(uiobj.curbuf)+1)
@@ -178,6 +240,7 @@ def InputLoop(uiobj):
 				uiobj.inputwin.clrtoeol()
 			elif len(uiobj.inbuf) > 0:
 				uiobj.inbuf = uiobj.inbuf[:-1]
+				uiobj.bufposition -= 1
 				uiobj.inputwin.addstr(1,1,uiobj.inbuf[-uiobj.width-2:])
 				if  len(uiobj.inbuf) - uiobj.width-2 > 0:
 					uiobj.screen.move(uiobj.height-2,len(uiobj.inbuf)+1-uiobj.width-1)
@@ -196,12 +259,17 @@ def InputLoop(uiobj):
 				uiobj.inbuf = "%s%s" % (uiobj.inbuf,str(chr(ch)))
 				uiobj.bufposition += 1
 			if len(uiobj.inbuf) >= uiobj.width-2:
-				tempbuf = uiobj.inbuf[len(uiobj.inbuf) - uiobj.width -2:]
+#				tempbuf = uiobj.inbuf[len(uiobj.inbuf) - uiobj.width -2:]
+#				tempbuf = uiobj.inbuf[:uiobj.width -2 - len(uiobj.inbuf)]
+#				tempbuf = uiobj.inbuf[uiobj.width -2 - len(uiobj.inbuf):]
+				tempbuf = uiobj.inbuf[-uiobj.width+2:]
+				sendText(uiobj,str(len(tempbuf)))
+				sendText(uiobj,str(uiobj.width-2))
 				uiobj.inputwin.addstr(1,1,tempbuf)
 				uiobj.screen.move(uiobj.height-2,uiobj.width-1)
-			elif len(uiobj.inbuf) < uiobj.width-2:
+			elif len(uiobj.inbuf) <= uiobj.width-2:
 				uiobj.inputwin.addstr(1,1,uiobj.inbuf)
-				uiobj.screen.move(uiobj.height-2,len(uiobj.inbuf)-1)
+				uiobj.screen.move(uiobj.height-2,len(uiobj.inbuf)+1)
 				uiobj.inputwin.clrtoeol()
 		ScreenRefresh(uiobj)
 
